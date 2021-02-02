a836c9dcd4f378cf8d4230e11558121a5fa49677
siddiq <msm2029@gmail.com>
2021-02-02 16:35:31 +0400

    renamed and added variables for posteriety

--- stopLidar.py
+++
@@ -1,8 +0,0 @@
-# Quick stop rplidar using script
-
-from rplidar import RPLidar
-lidar = RPLidar('COM8')
-
-lidar.stop()
-lidar.stop_motor()
-lidar.disconnect()
--- test.py
+++ test.py
@@ -1,48 +1,67 @@
+import sys
 import time
 from rplidar import RPLidar
-lidar = RPLidar('COM8')
-
-# info = lidar.get_info()
-# print(info)
-
-health = lidar.get_health()
-print(health)
-
-i = 0
-
-time.sleep(5)
+import math
 
 
-def stopLidar():
-    lidar.stop()
-    lidar.stop_motor()
-    lidar.disconnect()
+PORT_NAME = 'COM8'  # COM port used by the lidar
 
-def iterable_measurements():
-    for i, measurement in enumerate(lidar.iter_measurments()):
-        
-        if measurement[2] > 0 and measurement[2] < 90:
-            # if measurement[0] == True:
-            print(measurement)
-        # if i > 10:
-        #     stopLidar()
+stop = False
+templist = []
 
-def raw_measurements():
-    for measurement in (lidar.iter_measurments()):
-        if measurement[2] == 120:
-            print(measurement)
-        
-def iterable_scans():
-    for i, scan in enumerate(lidar.iter_scans()):
-        # print('%d: Got %d measurments' % (i, len(scan)))
-        print(i)
-        print(scan[0][0])
-        if i > 0:
-            stopLidar()
+# in mm
+tp_dist = 600 
+tp_dist_tolerance = 50
+tp_angle = 90 
+tp_angle_tolerance = 10
 
-# lidar.reset()
 
-# iterable_scans()
-# raw_measurements()
-iterable_measurements()
-# stopLidar()
+def scan(lidar):
+    global stop
+    while True:
+        counter = 0
+        print('Recording measurements... Press Crl+C to stop.')
+
+        for measurment in lidar.iter_measurments():
+            if stop == True:
+                lidar.stop()
+                lidar.stop_motor()
+                lidar.disconnect()
+                break
+
+            # t
+            if (measurment[2] > tp_angle - tp_angle_tolerance and measurment[2] < tp_angle + tp_angle_tolerance) :  # in angular range
+                templist.append(measurment[3])
+                
+            else:
+                if len(templist) != 0:
+                    avg_val = average(templist)
+
+                    if avg_val < tp_dist + tp_dist_tolerance and avg_val > tp_dist - tp_dist_tolerance:
+                        print(avg_val)
+                        # print(measurment[3])
+                    templist.clear()
+
+def average(lst):
+    return sum(lst) / len(lst) 
+
+
+def run():
+    '''Main function'''
+    lidar = RPLidar(PORT_NAME)
+    lidar.start_motor()
+    time.sleep(1)
+    info = lidar.get_info()
+    print(info)
+    try:
+        scan(lidar)
+    except KeyboardInterrupt:
+        stop = True
+        print('Stopping.')
+        lidar.stop()
+        lidar.stop_motor()
+        lidar.disconnect()
+
+
+if __name__ == '__main__':
+    run()
